80200b118238105c7effa8c7dbd73eed4fbae85f
PiyumalNipuna60 <nipuna315np@gmail.com>
2025-01-27 14:31:47 +0530

    cors issue fix

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*', 'https://judging-system.tequilasl.com',],
 
     'allowed_origins_patterns' => [],
 
